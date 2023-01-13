62f00f0280cc5b013da481792a579c5327ba4355
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-01-12 22:26:28 -0600

    obtener token del dispositivo

---
+++ lib/services/push_notifications_service.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+
+
+  static Future  initializeApp() async {
+
+    // Push Notifications
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+    print('Token: $token');
+
+    // Local Notifications
+  }
+
+}
