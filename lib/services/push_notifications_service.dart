2535c82f849ec6c161f52f03564907a8fdeb3dee
CristianAGR <cgr72677@udelasalle.edu.mx>
2023-01-13 19:54:57 -0600

    Reaccionar cuando recibimos una notificación

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
@@ -7,18 +9,28 @@ class PushNotificationService {
 
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
+  static final StreamController<String> _messageStream = StreamController.broadcast();
+
+  // suscribirse al stream
+  static Stream<String> get messagesStream => _messageStream.stream;
 
 
   static Future<void> _backgroundHandler( RemoteMessage message ) async {
-    print( 'onBackground Handler ${ message.messageId }');
+    //print( 'onBackground Handler ${ message.messageId }');
+    print( message.data );
+    _messageStream.add( message.notification?.title ?? 'No title');
   }
 
   static Future<void> _onMessageHandler( RemoteMessage message ) async {
-    print( 'onMessage Handler ${ message.messageId }');
+    //print( 'onMessage Handler ${ message.messageId }');
+    print( message.data );
+    _messageStream.add( message.notification?.title ?? 'No title');
   }
 
   static Future<void> _onMessageOpenApp( RemoteMessage message ) async {
-    print( 'onMessageOpenApp Handler ${ message.messageId }');
+    //print( 'onMessageOpenApp Handler ${ message.messageId }');
+    print( message.data );
+    _messageStream.add( message.notification?.title ?? 'No title');
   }
 
 
@@ -35,6 +47,10 @@ class PushNotificationService {
     FirebaseMessaging.onMessageOpenedApp.listen( _onMessageOpenApp );
 
     // Local Notifications
+  }
+
+  static closeStreams() {
+    _messageStream.close();
   }
 
 }
